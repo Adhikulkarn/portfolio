3e47d30754b117a38a1b9407541fac476ca29e63
Adhikulkarn <aditya.amcec23@gmail.com>
2026-05-24 22:25:52 +0530

    added all the apps

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'corsheaders',
+    'drf_spectacular',
+    'cloudinary',
+    'cloudinary_storage',
+    'accounts',
+    'projects',
+    'blogs',
+    'experience',
+    'skills',
+    'contact',
+    'resume',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -115,3 +129,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Portfolio CMS API',
+    'DESCRIPTION': 'Backend API for Portfolio CMS',
+    'VERSION': '1.0.0',
+}
+
+CORS_ALLOW_ALL_ORIGINS = True
+MEDIA_URL = '/media/'
